892b6afbb785d9f90e720d9d0461374f37de7126
rakeshh1990 <rakeshh1990@gmail.com>
2021-06-06 18:46:17 +0530

    Adding charts, structuring and basic styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :github_api_object, :get_user_details, if: -> { session[:access_token].present? }
 
   private
 
   def authorize
     redirect_to authorize_path if session[:access_token].nil?
+  end
+
+  def get_user_details
+    @current_user = @github_api_object.get_user_details
+
+    if @current_user.nil?
+      session[:access_token] = nil
+      redirect_to root_path
+    end
+  end
+
+  def github_api_object
+    @github_api_object ||= GithubApi.new(session[:access_token])
+    if @github_api_object.nil?
+      session[:access_token] = nil
+      redirect_to root_path
+    end
   end
 end
--- app/services/GithubApi.rb
+++ app/services/GithubApi.rb
@@ -6,6 +6,8 @@ class GithubApi
     response = RestClient.get('https://api.github.com/user', { params: { access_token: @access_token }, Authorization: @auth_token, accept: :json })
 
     @user_details = JSON.parse(response)
+  rescue
+    false
   end
 
   # fetch user information
@@ -25,5 +27,19 @@ class GithubApi
     response = RestClient.get(url, { params: { access_token: @access_token }, Authorization: @auth_token, accept: 'application/vnd.github.inertia-preview+json' })
 
     JSON.parse(response)
+  end
+
+  def get_repo_commits(repo_name)
+    url = "https://api.github.com/repos/#{@user_details['login']}/#{repo_name}/commits"
+    response = RestClient.get(url, { params: { access_token: @access_token }, Authorization: @auth_token, accept: 'application/vnd.github.inertia-preview+json' })
+
+    commits_data = JSON.parse(response)
+
+    chart_data = []
+    commits_data.group_by { |commit| commit['commit']['author']['name'] }.each do |author, commit|
+      chart_data << [author, commit.length]
+    end
+
+    chart_data
   end
 end
